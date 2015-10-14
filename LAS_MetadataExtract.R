f7eb93a1fbe4c9ea00c22ab619d0c7dcf0bb78f4
Mike Treglia <mtreglia@gmail.com>
2015-10-14 14:44:01 -0500

    Finalized working version of scripts, a couple example folders; includes a parallel and non-parallel version

--- LAS_MetadataExtract.R
+++ LAS_MetadataExtract.R
@@ -1,12 +1,25 @@
 #Set working directory
-#setwd("M:/OK_LiDAR/RCode_Testing")
-setwd("M:/OK_LiDAR/OK_LAS_data")
+setwd("M:/OK_LiDAR/RCode_Testing") #Practice folder (7 las files total)
+#setwd("M:/OK_LiDAR/OK_LAS_data") #Full data
+#setwd("M:/OK_LiDAR/OK_LAS_data/OK_DamRehab_Assessment_2011") #Actual data we'll work on, containing ~9000 las files
 
 #Create list of las files
-lf <- list.files(pattern="*.las$", full.name=TRUE, include.dirs=TRUE, recursive=TRUE)
+system.time(lf <- list.files(pattern="*.las$", full.name=TRUE, include.dirs=TRUE, recursive=TRUE))
+
+###If testing on a larger subset (the second and third setwd options), 
+### best to use the line below as the full list of files is HUGE (the above line takes ~5 min on full folder)
+#lf <- list.files(pattern="*.las$", full.name=TRUE, include.dirs=FALSE)[1:100]
 
 #Create dataframe to be filled in in the loop
-las.filedata <- data.frame(FileName=as.character(rep(NA, length(lf))),epsg=as.integer(rep(NA, length(lf))),NumPoints=as.integer(rep(NA, length(lf))),xmin=as.numeric(rep(NA, length(lf))),xmax=as.numeric(rep(NA, length(lf))),ymin=as.numeric(rep(NA, length(lf))), ymax=as.numeric(rep(NA, length(lf))), stringsAsFactors=FALSE)
+las.filedata <- data.frame(FileName=as.character(rep(NA, length(lf))),
+                           epsg=as.integer(rep(NA, length(lf))),
+                           NumPoints=as.integer(rep(NA, length(lf))),
+                           xmin=as.numeric(rep(NA, length(lf))),
+                           xmax=as.numeric(rep(NA, length(lf))),
+                           ymin=as.numeric(rep(NA, length(lf))), 
+                           ymax=as.numeric(rep(NA, length(lf))), 
+                           stringsAsFactors=FALSE)
+
 
 ##############
 #Fill in the dataframe by looping lasinfo through all las files in working directory (and subdirectory)
@@ -15,7 +28,7 @@ system.time(for(i in 1:length(lf)){
   
   las.filedata[i, "FileName"] <- paste(lf[i])
   
-  tmp <- system(paste('cmd /c lasinfo -no_vlrs "', lf[i], '"', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
+  tmp <- system(paste('cmd /c lasinfo "', lf[i], '"', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
   
   las.filedata[i, "epsg"] <- as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
   las.filedata[i,"NumPoints"] <- as.integer(unlist(strsplit(tmp[1], split="   *"))[3])
@@ -41,51 +54,25 @@ library(doParallel)
 cl<- makeCluster(8)
 registerDoParallel(cl)
 
-system.time(epsg <- foreach(i= 1:length(lf), .combine=rbind) %dopar% {
+system.time(las.metadata <- foreach(i= 1:length(lf), .combine=rbind) %dopar% {
   
   
   tmp <- system(paste('cmd /c lasinfo  "', lf[i], '" 2>&1', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
   FileName <- paste(lf[i])
-  epsg <- as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
-  NumPoints <- as.integer(unlist(strsplit(tmp[1], split="   *"))[3])
-  xmin <- as.numeric(unlist(strsplit(tmp[2], " * "))[6])
-  xmax <- as.numeric(unlist(strsplit(tmp[3], " * "))[6])
-  ymin <- as.numeric(unlist(strsplit(tmp[2], " * "))[7])
-  ymax <- as.numeric(unlist(strsplit(tmp[3], " * "))[7])
-  cbind(FileName, epsg, xmin, xmax, ymin, ymax)
+  epsg <- (substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
+  NumPoints <- (unlist(strsplit(tmp[1], split="   *"))[3])
+  xmin <- unlist(strsplit(tmp[2], " * "))[6]
+  xmax <- unlist(strsplit(tmp[3], " * "))[6]
+  ymin <- unlist(strsplit(tmp[2], " * "))[7]
+  ymax <- unlist(strsplit(tmp[3], " * "))[7]
+  cbind(FileName, epsg, NumPoints, xmin, xmax, ymin, ymax)
 }
 )
 
 stopCluster(cl)
 
 #view result
-epsg
+head(las.metadata)
 
-
-
-##############
-# Attempt to do this via parallel processing - first, only trying to extract the epsg code
-# Evan Linde found that cmd /c prior to lasinfo prints it out to stdout instead of the default stderr
-#############
-library(foreach)
-library(doParallel)
-
-cl<- makeCluster(8)
-registerDoParallel(cl)
-
-system.time(epsg <- foreach(i= 1:length(lf), .combine=rbind) %dopar% {
-
-  tmp <- system(paste('cmd /c lasinfo "', lf[i], '" 2>&1', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
-  as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
-}
-)
-
-stopCluster(cl)
-
-
-epsg
-
-
-
-
+#Last thing needed is to redefine column types
 
