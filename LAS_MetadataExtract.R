433906909e7300a9e88758659239ff96e1ead284
unknown <mtreglia@hpc-cloud-okey3.ad.okstate.edu>
2015-10-14 13:52:44 -0500

    Have code working in Parallel, thanks to Evan's Problem-Solving

--- LAS_MetadataExtract.R
+++ LAS_MetadataExtract.R
@@ -1,5 +1,6 @@
 #Set working directory
-setwd("M:/OK_LiDAR/RCode_Testing")
+#setwd("M:/OK_LiDAR/RCode_Testing")
+setwd("M:/OK_LiDAR/OK_LAS_data")
 
 #Create list of las files
 lf <- list.files(pattern="*.las$", full.name=TRUE, include.dirs=TRUE, recursive=TRUE)
@@ -14,7 +15,7 @@ system.time(for(i in 1:length(lf)){
   
   las.filedata[i, "FileName"] <- paste(lf[i])
   
-  tmp <- system(paste('lasinfo "', lf[i], '"', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
+  tmp <- system(paste('cmd /c lasinfo -no_vlrs "', lf[i], '"', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
   
   las.filedata[i, "epsg"] <- as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
   las.filedata[i,"NumPoints"] <- as.integer(unlist(strsplit(tmp[1], split="   *"))[3])
@@ -31,8 +32,8 @@ las.filedata
 
 
 ##############
-# Attempt to do this via parallel processing - first, only trying to extract the epsg code
-# Problem is occurring - only getting a list of NAs
+# Attempt to do this via parallel processing for all vars
+# Evan Linde found that cmd /c prior to lasinfo prints it out to stdout instead of the default stderr
 #############
 library(foreach)
 library(doParallel)
@@ -40,9 +41,18 @@ library(doParallel)
 cl<- makeCluster(8)
 registerDoParallel(cl)
 
-system.time(epsg <- foreach(i= 1:length(lf)) %dopar% {
-  tmp <- system(paste('lasinfo "', lf[i], '"', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
-  as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
+system.time(epsg <- foreach(i= 1:length(lf), .combine=rbind) %dopar% {
+  
+  
+  tmp <- system(paste('cmd /c lasinfo  "', lf[i], '" 2>&1', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
+  FileName <- paste(lf[i])
+  epsg <- as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
+  NumPoints <- as.integer(unlist(strsplit(tmp[1], split="   *"))[3])
+  xmin <- as.numeric(unlist(strsplit(tmp[2], " * "))[6])
+  xmax <- as.numeric(unlist(strsplit(tmp[3], " * "))[6])
+  ymin <- as.numeric(unlist(strsplit(tmp[2], " * "))[7])
+  ymax <- as.numeric(unlist(strsplit(tmp[3], " * "))[7])
+  cbind(FileName, epsg, xmin, xmax, ymin, ymax)
 }
 )
 
@@ -50,3 +60,32 @@ stopCluster(cl)
 
 #view result
 epsg
+
+
+
+##############
+# Attempt to do this via parallel processing - first, only trying to extract the epsg code
+# Evan Linde found that cmd /c prior to lasinfo prints it out to stdout instead of the default stderr
+#############
+library(foreach)
+library(doParallel)
+
+cl<- makeCluster(8)
+registerDoParallel(cl)
+
+system.time(epsg <- foreach(i= 1:length(lf), .combine=rbind) %dopar% {
+
+  tmp <- system(paste('cmd /c lasinfo "', lf[i], '" 2>&1', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
+  as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
+}
+)
+
+stopCluster(cl)
+
+
+epsg
+
+
+
+
+
