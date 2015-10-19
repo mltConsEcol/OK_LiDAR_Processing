d345c560bdb60d14c731e7d53df43acbae3678d3
Mike Treglia <mtreglia@gmail.com>
2015-10-19 11:02:32 -0500

    this is the final version that was run to create 'LAS_Metadata.csv'

--- LAS_MetadataExtract.R
+++ LAS_MetadataExtract.R
@@ -1,6 +1,6 @@
 #Set working directory
-setwd("M:/OK_LiDAR/RCode_Testing") #Practice folder (7 las files total)
-#setwd("M:/OK_LiDAR/OK_LAS_data") #Full data
+#setwd("M:/OK_LiDAR/RCode_Testing") #Practice folder (7 las files total)
+setwd("M:/OK_LiDAR/OK_LAS_data") #Full data
 #setwd("M:/OK_LiDAR/OK_LAS_data/OK_DamRehab_Assessment_2011") #Actual data we'll work on, containing ~9000 las files
 
 #Create list of las files
@@ -10,43 +10,8 @@ lf <- list.files(pattern="\\.las$", full.name=TRUE, include.dirs=TRUE, recursive
 ### best to use the line below as the full list of files is HUGE (the above line takes ~5 min on full folder)
 #lf <- list.files(pattern="\\.las$", full.name=TRUE, include.dirs=FALSE)[1:100]
 
-#Create dataframe to be filled in in the loop
-las.filedata <- data.frame(FileName=as.character(rep(NA, length(lf))),
-                           epsg=as.integer(rep(NA, length(lf))),
-                           NumPoints=as.integer(rep(NA, length(lf))),
-                           xmin=as.numeric(rep(NA, length(lf))),
-                           xmax=as.numeric(rep(NA, length(lf))),
-                           ymin=as.numeric(rep(NA, length(lf))), 
-                           ymax=as.numeric(rep(NA, length(lf))), 
-                           stringsAsFactors=FALSE)
-
 
 ##############
-#Fill in the dataframe by looping lasinfo through all las files in working directory (and subdirectory)
-#############
-system.time(for(i in 1:length(lf)){
-  
-  las.filedata[i, "FileName"] <- paste(lf[i])
-  
-  tmp <- system(paste('cmd /c lasinfo "', lf[i], '"', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
-  
-  las.filedata[i, "epsg"] <- as.integer(substring(tmp[4], 57, unlist(gregexpr(pattern =' - ',tmp[4]))[1]))
-  las.filedata[i,"NumPoints"] <- as.integer(unlist(strsplit(tmp[1], split="   *"))[3])
-  las.filedata[i, "xmin"] <- as.numeric(unlist(strsplit(tmp[2], " * "))[6])
-  las.filedata[i, "xmax"] <- as.numeric(unlist(strsplit(tmp[3], " * "))[6])
-  las.filedata[i, "ymin"] <- as.numeric(unlist(strsplit(tmp[2], " * "))[7])
-  las.filedata[i, "ymax"] <- as.numeric(unlist(strsplit(tmp[3], " * "))[7])
-  rm(tmp)
-}
-)
-
-#View result
-las.filedata
-
-
-##############
-# Attempt to do this via parallel processing for all vars
-# Evan Linde found that cmd /c prior to lasinfo prints it out to stdout instead of the default stderr
 #############
 library(foreach)
 library(doParallel)
@@ -55,7 +20,6 @@ cl<- makeCluster(8)
 registerDoParallel(cl)
 
 system.time(las.metadata <- foreach(i= 1:length(lf), .combine=rbind) %dopar% {
-  
   
   tmp <- system(paste('cmd /c lasinfo  "', lf[i], '" 2>&1', sep=''), intern=TRUE, wait=FALSE)[c(16,20,21,30)]
   FileName <- paste(lf[i])
@@ -74,6 +38,18 @@ stopCluster(cl)
 #view result
 head(las.metadata)
 
-#Last thing needed is to redefine column types appropriately, since dopar works with matrices
-# and all columns are stored as character data.
+#Convert to dataframe and assign columns correct type
+las.metadata <- as.data.frame(las.metadata, stringsAsFactors=FALSE)
 
+las.metadata$FileName <- as.character(las.metadata$FileName)
+las.metadata$epsg <- as.integer(las.metadata$epsg)
+las.metadata$NumPoints <- as.integer(las.metadata$NumPoints)
+las.metadata$xmin <- as.numeric(las.metadata$xmin)
+las.metadata$xmax <- as.numeric(las.metadata$xmax)
+las.metadata$ymin <- as.numeric(las.metadata$ymin)
+las.metadata$ymax <- as.numeric(las.metadata$ymax)
+
+str(las.metadata)
+head(las.metadata)
+
+write.csv(las.metadata,"LAS_Metadata.csv", row.names=FALSE)
