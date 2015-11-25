4365899ae1ad6194d797f669d2925ecd17028c2c
Mike Treglia <mtreglia@gmail.com>
2015-11-24 18:32:09 -0600

    Working on adding features to metadata extract (Unix) - only need to add time and time conversion stuff; also no real change to the working version... and added to Notes.txt

--- Metadata_Read_Edit/LAS_MetadataExtract_unix_test_addfeatures.R
+++ Metadata_Read_Edit/LAS_MetadataExtract_unix_test_addfeatures.R
@@ -6,7 +6,7 @@
 #
 # Example Code
 # setwd("../../../data/groups/OK_LiDAR/RCode_Testing")
-# system.time(test <- las.metadataExtract(getwd(), 6)) #don't write out metadata
+# system.time(test <- las.metadataExtract(getwd(), 8)) #don't write out metadata
 # system.time(test <- las.metadataExtract(path=getwd(), cores=6, out="testmeta1.csv"))
 ################
 
@@ -27,7 +27,7 @@ las.metadataExtract <- function(path=path, cores=cores, out=out){
     
       tmp <- system(paste('lasinfo  "', lf[i], '" -cd 2>&1', sep=''), intern=TRUE, wait=FALSE)
       
-      tmp2 <- list(length=5)
+      tmp2 <- list(length=9)
       
       tmp2[1] <- tmp[(grep(pattern='number of point records', tmp))]
       tmp2[2] <- tmp[(grep(pattern='min x y z', tmp))]
@@ -35,22 +35,35 @@ las.metadataExtract <- function(path=path, cores=cores, out=out){
       tmp2[4] <- ifelse(length(tmp[(grep(pattern='key 3072', tmp))])==0, NA, tmp[(grep(pattern='key 3072', tmp))])
       tmp2[5] <- tmp[(grep(pattern='spacing:', tmp))]
       tmp2[6] <- tmp[(grep(pattern='return_number', tmp))]
-
+      tmp2[7] <- ifelse(length(tmp[(grep(pattern='key 4098', tmp))])==0, NA, tmp[(grep(pattern='key 4098', tmp))])
+      tmp2[8] <- ifelse(length(tmp[(grep(pattern='key 3076', tmp))])==0, NA, tmp[(grep(pattern='key 3076', tmp))])
+      tmp2[9] <- ifelse(length(tmp[(grep(pattern='key 3073', tmp))])==0, NA, tmp[(grep(pattern='key 3073', tmp))])
+      tmp2[10] <- ifelse(length(tmp[(grep(pattern='key 4097', tmp))])==0, NA, tmp[(grep(pattern='key 4097', tmp))])
+      
+      
+      
+      
       
       tmp2 <- unlist(tmp2)
       
       FileName <- paste(lf[i])
-      epsg <- (substring(tmp2[4], 57, unlist(gregexpr(pattern =' - ',tmp2[4]))[1]))
+      epsgHoriz <- (substring(tmp2[4], 57, unlist(gregexpr(pattern =' - ',tmp2[4]))[1]))
+      epsgVert <- (substring(tmp2[7], 57, unlist(gregexpr(pattern =' - ',tmp2[7]))[1]))
+      VertUnitCode <- (substring(tmp2[8], 57, unlist(gregexpr(pattern =' - ',tmp2[8]))[1]))
       NumPoints <- (unlist(strsplit(tmp2[1], split="   *"))[3])
       xmin <- unlist(strsplit(tmp2[2], " * "))[6]
       xmax <- unlist(strsplit(tmp2[3], " * "))[6]
       ymin <- unlist(strsplit(tmp2[2], " * "))[7]
       ymax <- unlist(strsplit(tmp2[3], " * "))[7]
+      zmin <- unlist(strsplit(tmp2[2], " * "))[8]
+      zmax <- unlist(strsplit(tmp2[3], " * "))[8]
       PointSpacing <- unlist(strsplit(tmp2[5], " * "))[5]
       FirstReturnOnly <- ifelse(unlist(strsplit(tmp2[6], " * "))[4]==1, 1, 0)
+      HorizCRS_Txt <- (strsplit(tmp2[9], ": ")[[1]][2])
+      VertCRS_Txt <- (strsplit(tmp2[10], ": ")[[1]][2])
       
       
-      cbind(FileName, epsg, NumPoints, xmin, xmax, ymin, ymax, PointSpacing, FirstReturnOnly)
+      cbind(FileName, epsgHoriz, epsgVert, VertUnitCode, NumPoints, xmin, xmax, ymin, ymax, zmin, zmax, PointSpacing, FirstReturnOnly, HorizCRS_Txt, VertCRS_Txt)
     
   }
   
@@ -60,14 +73,20 @@ las.metadataExtract <- function(path=path, cores=cores, out=out){
   las.metadata <- data.frame(las.metadata, stringsAsFactors=FALSE)
   
   las.metadata$FileName <- as.character(las.metadata$FileName)
-  las.metadata$epsg <- as.integer(las.metadata$epsg)
+  las.metadata$epsgHoriz <- as.integer(las.metadata$epsgHoriz)
+  las.metadata$epsgVert <- as.integer(las.metadata$epsgVert)
+  las.metadata$VertUnitCode <- as.integer(las.metadata$VertUnitCode)
   las.metadata$NumPoints <- as.integer(las.metadata$NumPoints)
   las.metadata$xmin <- as.numeric(las.metadata$xmin)
   las.metadata$xmax <- as.numeric(las.metadata$xmax)
   las.metadata$ymin <- as.numeric(las.metadata$ymin)
   las.metadata$ymax <- as.numeric(las.metadata$ymax)
+  las.metadata$zmin <- as.numeric(las.metadata$zmin)
+  las.metadata$zmax <- as.numeric(las.metadata$zmax)
   las.metadata$PointSpacing <- as.numeric(las.metadata$PointSpacing)
   las.metadata$FirstReturnOnly <- as.integer(las.metadata$FirstReturnOnly)
+  las.metadata$HorizCRS_Txt <- as.character(las.metadata$HorizCRS_Txt)
+  las.metadata$VertCRS_Txt <- as.character(las.metadata$VertCRS_Txt)
   
   if(hasArg(out)==TRUE){
     write.csv(las.metadata, out, row.names=FALSE)
